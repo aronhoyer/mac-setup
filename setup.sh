1ca2adeae0188403e9ca7662476af12eb7fa9454
Aron Høyer <aron@hoyer.com>
2022-08-03 19:57:25 +0200

    add aliases

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 # Download .zshrc
 
 curl -fsSL https://raw.githubusercontent.com/aronhoyer/mac-setup/HEAD/.zshrc -o $HOME/.zshrc
+curl -fsSL https://raw.githubusercontent.com/aronhoyer/mac-setup/HEAD/.fs_aliases -o $HOME/.fs_aliases
+curl -fsSL https://raw.githubusercontent.com/aronhoyer/mac-setup/HEAD/.git_aliases -o $HOME/.git_aliases
+
+echo '' >> $HOME/.zshrc
+echo 'source $HOME/.fs_aliases' >> $HOME/.zshrc
+echo 'source $HOME/.git_aliases' >> $HOME/.zshrc
 
 xcode-select --install
 
@@ -10,23 +16,18 @@ xcode-select --install
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo '' >> $HOME/.zshrc
 echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> $HOME/.zshrc
 
 # Install formulae
 
-for formula in $(curl -fsSL https://raw.githubusercontent.com/aronhoyer/mac-setup/HEAD/formulae)
-do
-	brew install $formula
-done
+xargs brew install < "$(curl -fsSL https://raw.githubusercontent.com/aronhoyer/mac-setup/HEAD/formulae)"
 
 # Install casks
 
-for cask in $(curl -fsSL https://raw.githubusercontent.com/aronhoyer/mac-setup/HEAD/casks)
-do
-	brew install --cask $cask
-done
+xargs brew install --cask < "$(curl -fsSL https://raw.githubusercontent.com/aronhoyer/mac-setup/HEAD/casks)" 
 
-# Install NVM 
+# Install NVM
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
@@ -36,4 +37,7 @@ nvm alias default node
 # Install Rust
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y # this will run rustup without confirmation prompt
+
+echo '' >> $HOME/.zshrc
+echo 'export PATH="$PATH:$HOME/.cargo/bin:$(go env GOPATH)/bin:$(go env GOROOT)/bin"' >> $HOME/.zshrc
 
